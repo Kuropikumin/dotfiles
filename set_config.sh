5839a930c718fcc62c1ce5f3625b9bfaf2288f8f
Kuropikumin <wdbsf273@yahoo.co.jp>
2021-10-17 00:30:01 +0900

    - dein.toml への移行に伴い以下を微調整   - dein.toml   - vimrc_windows   - vimrc_linux   - set_config - coc.vimの導入   - dein.toml

--- set_config.sh
+++ set_config.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 ln -s ~/dotfiles/vimrc_linux ~/.vimrc
-mkdir -p ~/.vim/dein/repos/github.com/Shougo/
-git clone https://github.com/Shougo/dein.vim ~/.vim/dein/repos/github.com/Shougo/dein.vim
+mkdir -p ~/.vim/bundle/dein/repos/github.com/Shougo/
+ln -s ~/dotfiles/dein.toml ~/.vim/
+git clone https://github.com/Shougo/dein.vim ~/.vim/bundle/dein/repos/github.com/Shougo/dein.vim
 ln -s ~/dotfiles/screenrc ~/.screenrc
 
