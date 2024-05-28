a204f819221ba3f7507807387dd6216e5b034ce3
Kuropikumin <wdbsf273@yahoo.co.jp>
2024-05-28 10:22:14 +0900

    NeoVimでdein.vimを利用できるようにキャッシュ先のデータをシンボリックリンクでアクセスできるように変更

--- set_config.sh
+++ set_config.sh
@@ -6,3 +6,7 @@ ln -s ~/dotfiles/dein.toml ~/.vim/
 git clone https://github.com/Shougo/dein.vim ~/.vim/bundle/dein/repos/github.com/Shougo/dein.vim
 ln -s ~/dotfiles/screenrc ~/.screenrc
 
+# neovim setting
+mkdir -p ~/.config/nvim/
+ln -s ~/.vimrc ~/.config/nvim/init.vim
+ln -s ~/.vim/bundle/dein/.cache/.vimrc ~/.vim/bundle/dein/.cache/init.vim
